80c358ddb1befd8a2e898c9d9c5f1b713aa3bab7
ericywl <midnight2903@gmail.com>
2018-01-25 11:11:53 +0800

    added panels and blocks

--- multiballz/src/game.js
+++ multiballz/src/game.js
@@ -1,6 +1,7 @@
 var gameOptions = {
     scale: 0.5,
-    ballSpeed: 500
+    ballSpeed: 500,
+    blocksPerLine: 7
 };
 
 var game = new Phaser.Game(640 * gameOptions.scale, 960 * gameOptions.scale, Phaser.AUTO,
@@ -10,28 +11,40 @@ function preload() {
     game.load.image('ball', 'assets/ball.png');
     game.load.image('trajectory', 'assets/trajectory.png');
     game.load.image('panel', 'assets/panel.png');
+    game.load.image('block', 'assets/block.png');
 }
 
-var ball;
-var ballLaunched = false;
-var trajectory;
-
 function create() {
-    ball = createBall(game.world.centerX, game.world.height - 20);
-    trajectory = createTrajectory(ball.x, ball.y);
+    var blockSize = game.world.width / gameOptions.blocksPerLine;
 
-    // var panel = game.add.image(game.world.centerX, game.world.centerY, 'panel');
+    this.panel = game.add.image(game.world.centerX, game.world.height, 'panel');
+    this.panel.anchor.set(0.5, 1);
+    this.panel.width = game.world.width;
+    this.panel.height = blockSize;
+
+    this.ball = createBall(game.world.centerX, game.world.height - 20);
+    this.ballLaunched = false;
+    this.trajectory = createTrajectory(this.ball.x, this.ball.y);
+
+    this.blocks = game.add.group();
+    this.blocks.add(createBlock(game.world.centerX, game.world.centerY));
 }
 
 function update() {
     // change trajectory angle based on mouse pointer
     var pointerPos = game.input.activePointer.position;
-    var direction = Phaser.Math.angleBetween(ball.x, ball.y, pointerPos.x, pointerPos.y);
-    trajectory.position.set(ball.x, ball.y);
-    trajectory.angle = Phaser.Math.radToDeg(direction) + 90;
+    var direction = Phaser.Math.angleBetween(this.ball.x, this.ball.y, pointerPos.x, pointerPos.y);
+    this.trajectory.position.set(this.ball.x, this.ball.y);
+    this.trajectory.angle = Phaser.Math.radToDeg(direction) + 90;
 
     // shoot the ball on mouse press
     game.input.onDown.add(shootBall, this);
+    // kill block on collision
+    game.physics.arcade.collide(this.ball, this.blocks, removeBlock);
+}
+
+function removeBlock(ball, block) {
+    block.kill();
 }
 
 function createBall(x, y) {
@@ -46,28 +59,45 @@ function createBall(x, y) {
     return newBall;
 }
 
+function shootBall() {
+    if (this.ballLaunched) {
+        this.ball.x = game.world.centerX;
+        this.ball.y = game.world.height - 20;
+        this.ball.body.velocity.setTo(0, 0);
+
+        this.trajectory.visible = true;
+        this.ballLaunched = false;
+    } else {
+        var shootAngle = Phaser.Math.degToRad(this.trajectory.angle - 90);
+        this.ball.body.velocity.x = gameOptions.ballSpeed * Math.cos(shootAngle);
+        this.ball.body.velocity.y = gameOptions.ballSpeed * Math.sin(shootAngle);
+
+        this.trajectory.visible = false;
+        this.ballLaunched = true;
+    }
+}
+
 function createTrajectory(x, y) {
     var newTrajectory = game.add.image(x, y, 'trajectory');
     newTrajectory.anchor.set(0.5, 1);
     newTrajectory.scale.set(gameOptions.scale);
 
+    game.world.bringToTop(newTrajectory);
     return newTrajectory;
 }
 
-function shootBall() {
-    if (ballLaunched) {
-        ball.x = game.world.centerX;
-        ball.y = game.world.height - 20;
-        ball.body.velocity.setTo(0, 0);
+function createBlock(x, y) {
+    var newBlock = game.add.sprite(x, y, 'block');
+    newBlock.anchor.set(0.5);
+    newBlock.scale.set(gameOptions.scale);
 
-        trajectory.visible = true;
-        ballLaunched = false;
-    } else {
-        var shootAngle = Phaser.Math.degToRad(trajectory.angle - 90);
-        ball.body.velocity.x = gameOptions.ballSpeed * Math.cos(shootAngle);
-        ball.body.velocity.y = gameOptions.ballSpeed * Math.sin(shootAngle);
+    game.physics.arcade.enable(newBlock);
+    newBlock.body.immovable = true;
 
-        trajectory.visible = false;
-        ballLaunched = true;
-    }
+    return newBlock;
 }
+
+function addLine() {
+
+}
+
